731d1706b5fddf2b9145f950ddc2ae1b41eef445
diego fontecilla <mail@diegofontecilla.com>
2019-01-19 17:46:06 +0000

    Added unit test for #register_order: call #add_to_order_list on live_order_board

--- spec/live_order_board_spec.rb
+++ spec/live_order_board_spec.rb
@@ -1,15 +1,23 @@
 require 'live_order_board'
 
-describe do
+describe do LiveOrderBoard
 
   let(:fake_order) { double(:order) }
+  let(:new_order) { double(:new_order)}
   let(:live_order_board) { LiveOrderBoard.new(fake_order) }
 
   describe '#register_order' do
-    it 'to have received #new with 4 argumtents' do
+    it 'call #new in fake_order' do
       allow(fake_order).to receive(:new)
       live_order_board.register_order('fran', 4.5, 1500, 'BUY')
       expect(fake_order).to have_received(:new).with('fran', 4.5, 1500, 'BUY')
+    end
+
+    it 'call #add_to_order_list on live_order_board' do
+      allow(fake_order).to receive(:new)
+      allow(live_order_board).to receive(:add_to_order_list)
+      live_order_board.register_order('fran', 4.5, 1500, 'BUY')
+      expect(live_order_board).to have_received(:add_to_order_list)
     end
   end
 end
