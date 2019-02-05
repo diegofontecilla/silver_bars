56ae749ae6855c1bd88bd70c8338ecc5181d6257
diego fontecilla <mail@diegofontecilla.com>
2019-02-05 14:06:30 +0000

    Refactor on summary class

--- lib/live_order_board.rb
+++ lib/live_order_board.rb
@@ -4,39 +4,43 @@ require_relative 'summary'
 class LiveOrderBoard
   attr_reader :order_list, :summary
 
-  def initialize(order = Order, summary = Summary.new )
-    @order = order
-    @summary = summary
+  def initialize(order_class = Order, summary_class = Summary )
+    @order_class = order_class
+    @summary_class = summary_class
     @order_list = []
   end
 
   def register_order(user_id, order_quantity, price_per_kg, order_type)
-    new_order = @order.new(user_id, order_quantity, price_per_kg, order_type)
+    new_order = @order_class.new(user_id, order_quantity, price_per_kg, order_type)
     add_to_order_list(new_order)
   end
 
   def remove_order(user_id)
     order_list.each do |order|
-      order_list.delete(order) if order[:user_id] == user_id
+      order_list.delete(order) if order.user_id == user_id
     end
   end
 
   def get_summary_information
-    summary.get_summary_information(order_list)
+    summary = @summary_class.new(@order_list)
+    summary.merge(summary.sales)
+    # summary.merge(summary.purchases)
   end
 
   private
 
   def add_to_order_list(new_order)
-    order_list << new_order.get_order
+    order_list << new_order
   end
 end
 
 
 
-# live_order_board = LiveOrderBoard.new
-# live_order_board.register_order('robert', 8.5, 800, 'SELL')
-# live_order_board.register_order('fran', 4.5, 1500, 'BUY')
-# live_order_board.register_order('alberto', 2.5, 500, 'SELL')
-# live_order_board.register_order('al', 3.5, 1500, 'BUY')
-# p live_order_board.get_summary_information
+live_order_board = LiveOrderBoard.new
+live_order_board.register_order('fran', 4.0, 1500, 'BUY')
+live_order_board.register_order('alberto', 6.0, 900, 'BUY')
+live_order_board.register_order('erto', 6.0, 1500, 'BUY')
+live_order_board.register_order('alberto', 6.0, 500, 'SELL')
+live_order_board.register_order('alberto', 4.0, 500, 'SELL')
+live_order_board.register_order('al', 6.0, 800, 'SELL')
+p live_order_board.get_summary_information
--- lib/order.rb
+++ lib/order.rb
@@ -1,17 +1,1 @@
-
-class Order
-
-  def initialize(user_id, order_quantity, price_per_kg, order_type)
-    @current_order = {
-      user_id: user_id,
-      order_quantity: order_quantity,
-      price_per_kg: price_per_kg,
-      order_type: order_type
-    }
-  end
-
-  def get_order
-    return nil if @current_order == {}
-    @current_order
-  end
-end
+Order = Struct.new(:user_id, :order_quantity, :price_per_kg, :order_type)
